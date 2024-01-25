0ac2169809a2c75df937b1a79b8ce0881bdb51fc
Anaellen Silva <anaellen.silva123@gmail.com>
2024-01-24 23:00:57 -0300

    Terceiro commit

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=init_flask.py
-FLASK_ENV=development
--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import render_template
-
-@app.route('/')
-@app.route('/index')
-def index():
-    nome = 'Anaellen'
-    return render_template('index.html', nome)
